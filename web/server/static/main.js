47a9361f09340d75fca23e8ea319e6ef125e8c4c
Stas <kapilka06@gmail.com>
2019-08-05 13:59:44 +0300

    main.js

--- web/server/static/main.js
+++ web/server/static/main.js
@@ -1,19 +1,20 @@
-var canvas = document.getElementById("canvas");
-image1 = new MarvinImage();
-image1.load($('#img_src').val(), imageLoaded);
-image3 = new MarvinImage();
-image3.load($('#window_src').val(), imageLoaded);
+var c = document.getElementById("canvas1");
+var ctx = c.getContext("2d");
+var coords = JSON.parse($('#coords').val());
+var imageObj1 = new Image();
+var imageObj2 = new Image();
 
-var loaded = 0;
-var coords = JSON.parse($('#coords').val())[0];
-x = coords[0];//Math.floor((coords[0] + coords[2]) / 2);
-y = coords[1];//362 - Math.floor((coords[1] + coords[3]) / 2);
-console.log(x, y);
-function imageLoaded() {
-    if (++loaded == 2) {
-        var image = new MarvinImage(image1.getWidth(), image1.getHeight());
-        Marvin.scale(image3.clone(), image3, coords[3] - coords[1], coords[2] - coords[0])
-        Marvin.combineByAlpha(image1, image3, image, y, x);
-        image.draw(canvas);
+imageObj1.src = $('#img_src').val();
+imageObj1.onload = function () {
+    ctx.drawImage(imageObj1, 0, 0);
+    imageObj2.src = $('#window_src').val();
+    imageObj2.onload = function () {
+        for (let i = 0; i < coords.length; i++) {
+            const coodArr = coords[i];
+            ctx.drawImage(imageObj2, coodArr[1], coodArr[0], coodArr[3] - coodArr[1], coodArr[2] - coodArr[0]);
+        }
+
+        var img = c.toDataURL("image/png");
+        document.write('<img src="' + img + '" width="800" height="800"/>');
     }
-}
+};
