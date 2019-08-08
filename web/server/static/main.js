4c05f8e4199f110472c24f8a685d28242982fcb0
Stas <kapilka06@gmail.com>
2019-08-08 19:50:45 +0300

    rotation

--- web/server/static/main.js
+++ web/server/static/main.js
@@ -3,79 +3,7 @@ $(document).ready(function () {
         interval: 3600000
     });
 
-    // var c = document.getElementById("canvas1");
-    // var ctx = c.getContext("2d");
     var coords = JSON.parse($('#coords').val());
-
-    // function drawAllImages(windowSrc) {
-    //     console.log(windowSrc);
-    //     ctx.clearRect(0, 0, c.width, c.height);
-
-    //     var imageObj1 = new Image();
-    //     var imageObj2 = new Image();
-    //     var imageObj3 = new Image();
-
-    //     imageObj1.src = $('#img_src').val();
-    //     imageObj3.src = $('#glass_src').val();
-
-    //     imageObj1.onload = function () {
-    //         ctx.drawImage(imageObj1, 0, 0);
-    //         imageObj2.src = windowSrc;
-    //         imageObj2.onload = function () {
-    //             for (let i = 0; i < coords.length; i++) {
-    //                 const coodArr = coords[i];
-
-    //                 // ctx.translate(coodArr[1] / 2, coodArr[0] / 2);
-    //                 ctx.rotate(-20 + Math.PI / 2.0);
-    //                 // ctx.translate(-coodArr[1] / 2, -coodArr[0] / 2);
-    //                 // ctx.globalCompositeOperation = "darken";
-    //                 // ctx.drawImage(imageObj3, coodArr[1], coodArr[0], coodArr[3] - coodArr[1], coodArr[2] - coodArr[0]);
-    //                 // ctx.globalCompositeOperation = "xor";
-    //                 ctx.drawImage(imageObj2, coodArr[1], coodArr[0], coodArr[3] - coodArr[1], coodArr[2] - coodArr[0]);
-
-    //                 // ctx.restore();
-    //             }
-    //         }
-    //     };
-    // }
-
-    // drawAllImages($('#window_src').val());
-
-    // $('a.window-btn').on('click', function (e) {
-    //     e.preventDefault();
-    //     var windowSrc = $(this).attr("data-id");
-    //     drawAllImages("/static/images/windows/" + windowSrc);
-    // });
-
-    // var raster = new paper.Raster($('#img_src').val());
-    // var loaded = false;
-
-    // raster.on('load', function (res) {
-    //     console.log(res['event']['path'][0]['height']);
-    //     loaded = true;
-    //     paper.view.viewSize.width = res['event']['path'][0]['width'];
-    //     paper.view.viewSize.height = res['event']['path'][0]['height'];
-    //     raster.position = paper.view.center;
-
-    //     var windowImage = new Raster($('#glass_src').val());
-    //     for (let i = 0; i < coords.length; i++) {
-    //         const coodArr = coords[i];
-    //         // ctx.drawImage(imageObj2, coodArr[1], coodArr[0], coodArr[3] - coodArr[1], coodArr[2] - coodArr[0]);
-
-    //         // Move the raster to the center of the view
-    //         windowImage.position = new Point(coodArr[0], coodArr[3]);
-
-    //         // Scale the raster by 50%
-    //         windowImage.width = coodArr[3] - coodArr[1];
-    //         windowImage.height = coodArr[2] - coodArr[0];
-    //         debugger;
-    //         break;
-    //         // Rotate the raster by 45 degrees:
-    //         // windowImage.rotate(45);
-    //     }
-    // });
-
-
 
     function drawAllImages(windowSrc) {
         Konva.Image.fromURL($('#img_src').val(), function (houseObj) {
@@ -86,6 +14,23 @@ $(document).ready(function () {
                 container: 'ctx-container',
                 width: ctxWidth,
                 height: ctxHeight
+            });
+            stage.on('click tap', function (e) {
+                // if click on empty area - remove all transformers
+                if (e.target === stage) {
+                    stage.find('Transformer').destroy();
+                    layer.draw();
+                    return;
+                }
+                // remove old transformers
+                // TODO: we can skip it if current rect is already selected
+                stage.find('Transformer').destroy();
+
+                // create new transformer
+                var tr = new Konva.Transformer();
+                layer.add(tr);
+                tr.attachTo(e.target);
+                layer.draw();
             });
 
             var layer = new Konva.Layer();
@@ -101,7 +46,8 @@ $(document).ready(function () {
                         x: coodArr[1],
                         y: coodArr[0],
                         width: coodArr[3] - coodArr[1],
-                        height: coodArr[2] - coodArr[0]
+                        height: coodArr[2] - coodArr[0],
+                        draggable: true
                     };
 
                     glassObj.setAttrs(opts);
